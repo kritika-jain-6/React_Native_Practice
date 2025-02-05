bcc8a9ca4ad4473db2f7e87ba284e68ddbc4b5d4
Kritika Jain <kritikajain614@gmail.comgit config --global user.email kritikajain614@gmail.comgit config --global user.email kritikajain614@gmail.com>
2025-02-05 20:28:06 +0530

    React Native upto 4feb

--- Project01/App.jsx
+++ Project01/App.jsx
@@ -1,20 +1,204 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import Nav from "./src/Components/Nav";
+import TabNav from "./src/Components/TabNavigation/TabNav";
+import TopTabNav from "./src/Components/TabNavigation/TopTabNavigation/TopTabNav";
+import Login1 from "./src/Components/Project1/Login1";
+// import ApiTry from "./src/Components/TabNavigation/ApiTry";
+import ApiCall from "./src/Components/API/ApiCall";
+import ApiList from "./src/Components/API/ApiList";
+import ApiFlatList from "./src/Components/API/ApiFlatList";
 import Asyncst from './src/events/asyncst';
 import Gestureevents from './src/events/gestureevents';
 import Scrollevents from './src/events/scrollevents';
 import Touchevents from './src/events/touchevents';
 
+// import { Text } from "react-native-gesture-handler";/
+import {Text,View} from "react-native"
+import LinearBox from "./src/Components/Linera Gradient/LinearBox";
+import LinearBg from "./src/Components/Linera Gradient/LinearBg";
+import LinearText from "./src/Components/Linera Gradient/LinearText";
+import Ufini from "./src/Components/Ucampus/Ufini";
+import FCam from "./src/Components/Ucampus/FCam";
+import GestureHand from "./src/Components/Events & Listner Handling/GestureHand";
+import TouchEvent from "./src/Components/Events & Listner Handling/Hand Touching events/TouchEvent";
+
+import Async from "./src/Components/Async Storage/Async";
+import { DefaultTheme } from "@react-navigation/native";
+import { ScrollView } from "react-native-gesture-handler";
+
+
+
 const App = () => {
-  return (
-    <View>
-      <Asyncst/>
+  return(
+    <>
+
+    
+    <Ufini/>
+    {/* <UCampus/> */}
+
+    {/* <Async/> */}
+    
+    {/* <TouchEvent/> */}
+    {/* <GestureHand/> */}
+   
+    {/* <FCam/> */}
+    {/* <Ufini/> */}
+    {/* <LinearText/> */}
+    {/* <LinearBg/> */}
+
+    {/* <LinearBox/> */}
+    {/* <Text>hi</Text> */}
+   {/* <UCampus/> */}
+    {/* <ApiFlatList/> */}
+    {/* <ApiList/> */}
+{/* <ApiCall/> */}
+    {/* <ApiTry/> */}
+    {/* <UCampus/> */}
+
+{/* <Login1/> */}
+    {/* <ApiCall/> */}
+    {/* <TopTabNav/> */}
+    {/* <TabNav/> */}
+    {/* <Nav/> */}
+    <Asyncst/>
       <Gestureevents/>
       <Scrollevents/>
       <Touchevents/>
-      
-    </View>
+    </>
   )
-}
+};
 
-export default App
+export default App;
+
+
+
+
+
+
+
+// import { View, Text } from 'react-native'
+// import React from 'react'
+// import Flex from './src/Components/Flex'
+
+// const App = () => {
+//   return (
+//     <>
+//     <Flex/>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+// import { View, Text, Button, ScrollView, Pressable, StatusBar } from 'react-native';
+// import React from 'react';
+// import First from './src/Components/First';
+// import JSX from './src/Components/JSX';
+// import UserData from './src/Components/UserData';
+// import OnPress from './src/Components/OnPress';
+// import { State,Counter,Counter2, LightSwitch, ThemeToggler} from './src/Components/State';
+// import { Props, Props2, Props3, User, User1 } from './src/Components/Props';
+// import { Styling, Styling1, Styling2, Styling3 } from './src/Components/Styling';
+// import InputArea from './src/Components/InputArea';
+// import Form from './src/Components/Form';
+// import List from './src/Components/List';
+// import ListMap from './src/Components/ListMap';
+// import GridStatic from './src/Components/GridStatic';
+// import GridDynamic from './src/Components/GridDynamic';
+// // import CompoFlagList from './src/Components/CompoFlagList';
+// import ClassComp from './src/Components/ClassComp';
+// import Hooks from './src/Components/Hooks';
+// import Hide from './src/Components/Hide';
+// import Unmount from './src/Components/Unmount';
+// import Loop from './src/Components/Loop';
+// import ListSect from './src/Components/ListSect';
+// import Responsive from './src/Components/Responsive';
+// import WishList from './src/Components/WishList';
+// // import Ui from './src/Components/UI';
+// // import Wishlist from './src/Components/WishList';
+// //import Touch from './src/Components/Touch';
+// import Radio from './src/Components/Radio';
+// import DynamicRadio from './src/Components/DynamicRadio';
+// import ActivityIndi from './src/Components/ActivityIndi';
+// import Dialog from './src/Components/Dialog';
+// import PressAb from './src/Components/PressAb';
+// import StatusB from './src/Components/StatusB';
+// import Platf from './src/Components/Platf';
+// import Web from './src/Components/Web';
+// import CustomDialog from './src/Components/CustomDialog';
+
+// const App = () => {
+//   return (
+//     // <ScrollView>
+  
+//     <View>
+//       {/* <Text style={ { fontSize: 31, color: 'red' } }>Hello!!! Developer</Text> */}
+//       {/* <First/>
+//       <JSX/>
+//       <UserData/>
+
+
+//       <OnPress/>
+      
+//       <State/>
+//       <Counter/>
+//       <Counter2/>
+//       <LightSwitch/>
+
+//       <ThemeToggler/>
+//       <Props/>
+//       <Props2/>
+//       <Props3/>
+
+//       <Styling/>
+//       <Styling1/>
+//       <Styling2/>
+     
+//       <Styling3/>
+//       <InputArea/>
+
+//       <Form/>
+
+//       <List/>
+//       <ListMap/>
+//       <GridStatic/>
+//       <GridDynamic/> 
+//     <CompoFlagList/> 
+
+//     <ClassComp/> 
+//     <Hooks/>
+//       <Hide/>
+
+//   <Unmount/>
+//   <Loop/>
+//   <ListSect/>
+
+//     {/* <CustomDialog/> */}
+//     {/* <Web/> */}
+//     {/* <Platf/> */}
+//     {/* <StatusB/> */}
+
+//     {/* <PressAb/> */}
+
+
+//     {/* <Dialog/> */}
+//     {/* <ActivityIndi/> */}
+//     {/* <DynamicRadio/> */}
+//   */}
+
+//   <Responsive/>
+
+//     </View>  
+
+// // </ScrollView> 
+//   )
+// };
+
+// export default App;
